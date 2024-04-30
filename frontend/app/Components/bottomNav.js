6efa60a6e4ef8514b304f67bb7aaa580333db476
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-04-30 10:49:59 +0300

    adding prop to navabar

--- frontend/app/Components/bottomNav.js
+++ frontend/app/Components/bottomNav.js
@@ -6,21 +6,22 @@ import { MaterialIcons } from '@expo/vector-icons';
 const BottomNavigationBar = ({ navigation }) => {
     return (
         <View style={styles.navContainer}>
-            {navItem(require('../../assets/home-icon.png'), 'Home', 'Home', navigation)}
-            {navItem(require('../../assets/pts-icon.png'), 'PTs', 'PTs', navigation)}
-            {navItem(require('../../assets/setup-icon.png'), 'Setup', 'Setup', navigation, true)} 
-            {navItem(require('../../assets/progress-icon.png'), 'Progress', 'Progress', navigation)}
-            {navItem(require('../../assets/profile-icon.png'), 'Profile', 'Profile', navigation)}
+            {navItem(require('../../assets/home-icon.png'), 'Home', 'Home', navigation,currentTab === 'Home')}
+            {navItem(require('../../assets/pts-icon.png'), 'PTs', 'PTs', navigation,currentTab === 'PTs')}
+            {navItem(require('../../assets/setup-icon.png'), 'Setup', 'Setup', navigation, currentTab === 'Setup')} 
+            {navItem(require('../../assets/progress-icon.png'), 'Progress', 'Progress', navigation,currentTab === 'Progress')}
+            {navItem(require('../../assets/profile-icon.png'), 'Profile', 'Profile', navigation,currentTab === 'Profile')}
         </View>
     );
 };
 
-const navItem = (iconSource, label, navTarget, navigation, isCurrent = false) => (
+const navItem = (iconSource, label, navTarget, navigation, isCurrent) => (
     <TouchableOpacity style={styles.navItem} onPress={() => navigation.navigate(navTarget)}>
         <Image source={iconSource} style={[styles.icon, isCurrent && styles.iconCurrent]} />
         <Text style={[styles.navText, isCurrent && styles.navTextCurrent]}>{label}</Text>
     </TouchableOpacity>
 );
+
 
 const styles = StyleSheet.create({
     navContainer: {
