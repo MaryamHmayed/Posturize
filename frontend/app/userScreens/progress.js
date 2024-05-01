2fbf567ce5b189fd48b32d8768aeec01f410e56e
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-05-01 22:18:58 +0300

    frontend- styling calender

--- frontend/app/userScreens/progress.js
+++ frontend/app/userScreens/progress.js
@@ -8,10 +8,38 @@ const ProgressScreen = () => {
             <View style={styles.header}>
                 <Text style={styles.headerTitle}>Progress</Text>
             </View>
-            <View style={styles.calendarContainer}>
-                <Calendar/>
+            <ScrollView style={styles.calendarContainer}>
+                <Calendar
+                theme={{
+                    backgroundColor: '#2B2B2B',
+                    calendarBackground: '#2B2B2B',
+                    textSectionTitleColor: 'white',
+                    textSectionTitleDisabledColor: 'gray',
+                    selectedDayBackgroundColor: '#FFA500',
+                    selectedDayTextColor: '#ffffff',
+                    todayTextColor: '#FFA500',
+                    dayTextColor: 'white',
+                    textDisabledColor: 'gray',
+                    dotColor: '#00adf5',
+                    selectedDotColor: '#ffffff',
+                    arrowColor: 'orange',
+                    disabledArrowColor: '#d9e1e8',
+                    monthTextColor: 'white',
+                    indicatorColor: 'blue',
+                    textDayFontWeight: '300',
+                    textMonthFontWeight: 'bold',
+                    textDayHeaderFontWeight: '300',
+                    textDayFontSize: 16,
+                    textMonthFontSize: 16,
+                    textDayHeaderFontSize: 16
+                }}
+                
+                
+                
+                />
 
-            </View>
+
+            </ScrollView>
 
         </ScrollView>
     )}
@@ -23,7 +51,7 @@ const ProgressScreen = () => {
             backgroundColor: '#2B2B2B',
         },
         header: {
-            padding: 20,
+            
             alignItems: 'center',
             borderBottomWidth: 1,
             borderBottomColor: 'grey',
@@ -32,9 +60,14 @@ const ProgressScreen = () => {
             color: 'white',
             fontSize: 20,
             fontWeight: 'bold',
+            paddingTop:50,
+            paddingVertical:20
         },
         calendarContainer: {
-            marginBottom: 10,
+            marginHorizontal: 30,
+            height:180
+            
+
         },
     })
 
