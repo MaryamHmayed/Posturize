45daffec523fe1a4c3ba0d6799c27cc2553c1235
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-05-06 13:10:58 +0300

    backend- adding updating profile API under physiotherapist middlware

--- backend/app/Http/Controllers/PhysiotherapistController.php
+++ backend/app/Http/Controllers/PhysiotherapistController.php
@@ -17,17 +17,17 @@ class PhysiotherapistController extends Controller
     
         $user = User::find(Auth::id());
     
-        // Ensure a user is authenticated
         if (!$user) {
             return response()->json(['message' => 'No authenticated user found'], 404);
         }
     
-        // Update the user with the new data
         $user->update($req->only(['bio', 'location', 'phone_number']));
-    
-        // Return a success response
+      
         return response()->json(['message' => 'Profile updated successfully', 'user' => $user]);
 
     }
+
+
+    
        
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PhysiotherapistController;
+use App\Http\Middleware\PhysiotherapistMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -9,6 +11,10 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
     Route::post('logout', 'logout');
-    Route::post('chair','addChair');
-
 });
+
+
+Route::prefix('pt')->middleware(['api', 'auth:api', PhysiotherapistMiddleware::class])
+    ->group(function () {
+        Route::post('update_profile', [PhysiotherapistController::class, 'updateProfile']);
+    });
