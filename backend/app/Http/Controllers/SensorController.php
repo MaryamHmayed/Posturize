b8ae2c5e33d13ebcf27c50fe69bfbaa9d121a15a
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-05-07 00:00:36 +0300

    backend- getting values of sensors

--- backend/app/Http/Controllers/SensorController.php
+++ backend/app/Http/Controllers/SensorController.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Sensors_Data;
 use Illuminate\Http\Request;
 
 class SensorsController extends Controller
 {
     public function store(Request $request)
-    {
+{
     
     $data = $request->validate([
         'session_id' => 'required|integer|exists:sessions,id',
@@ -17,6 +17,19 @@ class SensorsController extends Controller
         'sensor_values.*.timestamp' => 'required|date',
     ]);
 
+
+    foreach ($data['sensor_values'] as $sensor_value) {
+        $sensorData = Sensors_Data::create([
+            'sensor_id' => $sensor_value['sensor_id'],
+            'sensor_value' => $sensor_value['value'],
+            'timestamp' => $sensor_value['timestamp'],
+           
+        ]);
+
+ 
+       
     }
 
+    return response()->json(['message' => 'Data uploaded successfully']);
+}
 }
