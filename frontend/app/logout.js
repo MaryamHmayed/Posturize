a3879e6d52ee2ff8163e567b238fae1bb0358bba
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-05-07 21:26:23 +0300

    frontend -updating logout

--- frontend/app/logout.js
+++ frontend/app/logout.js
@@ -1,32 +1,29 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
+import { useNavigation } from '@react-navigation/native';
 
-const logoutUser = async ({navigation}) => {
-   
-    try {
- 
-        const token = await AsyncStorage.getItem('userToken');
-        if (!token) {
-          console.log('Retrieved token:', token);
-          console.error("No token found, cannot logout.");
-          return;
-        }
-        const response = await axios.post('http://192.168.1.109:8000/api/logout', {}, {
-          headers: {
-            'Authorization': `Bearer ${token}`
-          }
-        });
-    
-        if (response.status === 200) {
-          console.log('Logout successful');
-          await AsyncStorage.removeItem('userToken'); 
-          await AsyncStorage.removeItem('userType'); 
-          navigation.navigate('Login');
-        }
-      } catch (error) {
-        console.error('Logout failed:', error);
-       
-      }
-    };
+const logoutUser = async () => {
+  const navigation = useNavigation(); 
+  try {
+    const token = await AsyncStorage.getItem('userToken');
+    if (!token) {
+      console.log('No token found, cannot logout.');
+      return;
+    }
 
-    export default logoutUser;
+    const response = await axios.post('http://192.168.1.109:8000/api/logout', {}, {
+      headers: { 'Authorization': `Bearer ${token}` }
+    });
+
+    if (response.status === 200) {
+      console.log('Logout successful');
+      await AsyncStorage.removeItem('userToken');
+      await AsyncStorage.removeItem('userType');
+      navigation.navigate('Login'); 
+    }
+  } catch (error) {
+    console.error('Logout failed:', error);
+  }
+};
+
+export default logoutUser;
