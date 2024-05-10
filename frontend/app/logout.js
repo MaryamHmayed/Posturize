a18b668df44ba5271e01ac6c0ddaf68b3184413a
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-05-10 19:09:54 +0300

    frontend- editing logout function

--- frontend/app/logout.js
+++ frontend/app/logout.js
@@ -1,29 +1,34 @@
+import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
-import { useNavigation } from '@react-navigation/native';
 
-const logoutUser = async () => {
-  const navigation = useNavigation(); 
-  try {
-    const token = await AsyncStorage.getItem('userToken');
-    if (!token) {
-      console.log('No token found, cannot logout.');
-      return;
+const useLogout = () => {
+  const navigation = useNavigation();
+
+  const logoutUser = async () => {
+    try {
+      const token = await AsyncStorage.getItem('userToken');
+      if (!token) {
+        console.log('No token found, cannot logout.');
+        return;
+      }
+
+      const response = await axios.post('http://192.168.1.109:8000/api/logout', {}, {
+        headers: { 'Authorization': `Bearer ${token}` }
+      });
+
+      if (response.status === 200) {
+        console.log('Logout successful');
+        await AsyncStorage.removeItem('userToken');
+        await AsyncStorage.removeItem('userType');
+        navigation.navigate('Login'); 
+      }
+    } catch (error) {
+      console.error('Logout failed:', error);
     }
+  };
 
-    const response = await axios.post('http://192.168.1.109:8000/api/logout', {}, {
-      headers: { 'Authorization': `Bearer ${token}` }
-    });
-
-    if (response.status === 200) {
-      console.log('Logout successful');
-      await AsyncStorage.removeItem('userToken');
-      await AsyncStorage.removeItem('userType');
-      navigation.navigate('Login'); 
-    }
-  } catch (error) {
-    console.error('Logout failed:', error);
-  }
+  return { logoutUser };
 };
 
-export default logoutUser;
+export default useLogout;
