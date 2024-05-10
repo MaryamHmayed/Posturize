a334fb57fea8bfbc85e60ac913129bdd91085e98
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-05-10 07:08:19 +0300

    frontend- updating user context to handle id

--- frontend/app/userContext.js
+++ frontend/app/userContext.js
@@ -4,6 +4,7 @@ const UserContext = createContext(null);
 
 export const UserProvider = ({ children }) => {
     const [user, setUser] = useState({
+        id : null,
         email: '',
         username: '',
         password: '',
