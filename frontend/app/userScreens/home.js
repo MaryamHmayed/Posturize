92b22f788955699af5c0ecddb7749ccb320b51cf
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-05-14 20:40:51 +0300

    frontend- add register push notifications function

--- frontend/app/userScreens/home.js
+++ frontend/app/userScreens/home.js
@@ -71,7 +71,36 @@ const HomeScreen = () => {
         addNotification(notificationContent); 
     };
 
-   
+    const registerForPushNotificationsAsync = async () => {
+        let token;
+        if (Constants.isDevice) {
+            const { status: existingStatus } = await Notifications.getPermissionsAsync();
+            let finalStatus = existingStatus;
+            if (existingStatus !== 'granted') {
+                const { status } = await Notifications.requestPermissionsAsync();
+                finalStatus = status;
+            }
+            if (finalStatus !== 'granted') {
+                Alert.alert('Failed to get push token for push notification!');
+                return;
+            }
+            token = (await Notifications.getExpoPushTokenAsync()).data;
+            console.log('Expo push token:', token);
+        } else {
+            Alert.alert('Must use physical device for Push Notifications');
+        }
+
+        if (Platform.OS === 'android') {
+            Notifications.setNotificationChannelAsync('default', {
+                name: 'default',
+                importance: Notifications.AndroidImportance.MAX,
+                vibrationPattern: [0, 250, 250, 250],
+                lightColor: '#FF231F7C',
+            });
+        }
+
+        return token;
+    };
 
     const postureAttributes = {
         good: {
