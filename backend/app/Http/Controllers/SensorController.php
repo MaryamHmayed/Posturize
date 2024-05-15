af98fdfd3ac62002c87b5e808ae3e9820f933cc8
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-05-15 21:55:07 +0300

    backend- validating data to be stored in sensor controller

--- backend/app/Http/Controllers/SensorController.php
+++ backend/app/Http/Controllers/SensorController.php
@@ -1,15 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Sensors_Data;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-use App\Models\Posture_Data;
-use App\Models\Session;
+use App\Models\User;
+use App\Models\Chair;
 class SensorsController extends Controller
 {
     public function store(Request $request)
 {
-    
+    $user = User::find(Auth::id());
+
+
+    $validatedData = $request->validate([
+        'date' => 'required|date',
+        'totalTimeTracked' => 'required|numeric',
+        'postureDurations' => 'required|array',
+        'posturePercentages' => 'required|array',
+    ]);
 }
 
 
