706425e380c42398cc68053fbc5b25d17d3eec15
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-05-15 21:19:17 +0300

    frontend- starting with sensor controller to store posture data

--- backend/app/Http/Controllers/SensorController.php
+++ backend/app/Http/Controllers/SensorController.php
@@ -10,44 +10,7 @@ class SensorsController extends Controller
     public function store(Request $request)
 {
     
-    $data = $request->validate([
-        'session_id' => 'required|integer|exists:sessions,id',
-        'sensor_values' => 'required|array',
-        'sensor_values.*.sensor_id' => 'required|integer|exists:sensors,id',
-        'sensor_values.*.value' => 'required|numeric',
-        'sensor_values.*.timestamp' => 'required|date',
-    ]);
-
-
-    foreach ($data['sensor_values'] as $sensor_value) {
-        $sensorData = Sensors_Data::create([
-            'sensor_id' => $sensor_value['sensor_id'],
-            'sensor_value' => $sensor_value['value'],
-            'timestamp' => $sensor_value['timestamp'],
-           
-        ]);
-
-        $this->evaluatePosture($sensorData);
-       
-    }
-
-    return response()->json(['message' => 'Data uploaded successfully']);
 }
-
-    private function evaluatePosture($sensorData)
-    {
-        
-        $threshold = 500; 
-        $postureStatus = ($sensorData->sensor_value > $threshold) ? 'bad' : 'good';
-       
-
-
-        Posture_Data::create([
-            // 'session_id' => $session->id, 
-            'posture_status' => $postureStatus,
-            
-        ]);
-    }
 
 
 
