031952b876dad402fd0e155618ee466afb0c2b9b
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-05-16 22:27:55 +0300

    frontend-  adding render item

--- frontend/app/userScreens/notifications.js
+++ frontend/app/userScreens/notifications.js
@@ -6,5 +6,13 @@ import { useUser } from "../userContext";
 const NotificationsScreen = ()=>{
     const {notifications} = useUser();
 
-    
+    const renderItem = ({item}) => (
+        <View style = {styles.notificationsContainer}> 
+            <Text style = {styles.title}>{item.title}</Text>
+            <Text style={styles.body}>{item.body}</Text>
+            <Text style={styles.timestamp}>{new Date (item.data.timestamp).toLocaleString()}</Text>
+        
+        
+        </View>
+    )
 }
