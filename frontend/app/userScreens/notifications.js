fe738415f2f18c9c3c658a0ce8ddc780f4be319f
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-05-16 22:22:14 +0300

    frontend- sarting with notifications screen

---
+++ frontend/app/userScreens/notifications.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { useUser } from "../userContext";
+
+
+const NotificationsScreen = ()=>{
+    const {notifications} = useUser();
+
+    
+}
