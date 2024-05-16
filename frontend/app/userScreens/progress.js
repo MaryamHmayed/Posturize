d76703ff150792600f50c04262770890babc8111
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-05-16 17:19:49 +0300

    frontend- defining format elapsed time function

--- frontend/app/userScreens/progress.js
+++ frontend/app/userScreens/progress.js
@@ -37,7 +37,11 @@ const ProgressScreen = () => {
 
   
 
-    
+    const formatElapsedTime = (seconds) => {
+        const hours = Math.floor(seconds / 3600);
+        const minutes = Math.floor((seconds % 3600) / 60);
+        return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+    };
 
     const renderStats = () => {
         if (!data || !selectedDate) {
