c0be85de46b1560777ecab6816c89e816de4e270
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-05-19 06:00:10 +0300

    backend- creating user and chair in add chair test

--- backend/tests/Feature/AddChairTest.php
+++ backend/tests/Feature/AddChairTest.php
@@ -1,20 +1,61 @@
 <?php
 
 namespace Tests\Feature;
-
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
+use App\Models\Chair;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 class AddChairTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    use DatabaseTransactions;
+
+    public function test_add_chair_success()
+    {
+       
+        $user = User::factory()->create([
+            'username' => 'testuser',
+            'email' => 'test@example.com',
+            'password' => bcrypt('password123'),
+        ]);
+        $token = auth('api')->login($user);
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->postJson('/api/add_chair', [
+            'chair_name' => 'ErgoChair',
+        ]);
+
+        $response->assertStatus(201);
+        $response->assertJson([
+            'message' => 'New chair created successfully',
+            'chair' => [
+                'chair_name' => 'ErgoChair',
+                'user_id' => $user->id,
+            ],
+        ]);
     }
+
+    public function test_add_chair_already_assigned_same_name()
+    {
+
+        $user = User::factory()->create([
+            'username' => 'testuser',
+            'email' => 'test@example.com',
+            'password' => bcrypt('password123'),
+        ]);
+
+        $chair = Chair::factory()->create([
+            'chair_name' => 'ErgoChair',
+            'user_id' => $user->id,
+        ]);
+
+        $token = auth('api')->login($user);
+
+       
+      
+    }
+
+
 }
